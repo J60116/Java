cd0e80a213ec795d16b07ef3cd3c988a694639b9
mow <154735003+J60116@users.noreply.github.com>
2024-02-09 13:13:27 +0900

    Update Pokemon.java

--- testPokemon/Pokemon.java
+++ testPokemon/Pokemon.java
@@ -1,75 +1,78 @@
 package testPokemon;
 
+import java.util.Random;
+
 abstract class Pokemon {
 	
-	//進化前、進化後
-
 	//タイプ
 	final String[] ARRAY_TYPE = {"NORMAL","FIRE","WATER","ELECTRIC","GRASS","ICE","FIGHTING","POISON","GROUND","FLYING","PSYCHIC","BUG","ROCK","GHOST","DRAGON","DARK","STEEL","FAIRY"};
-    //ボールの種類(0:野生、4:敵)
-    final String[] ARRAY_BALL = {"Wild","Monster ball","Super ball","Master ball","Enemy"};
-    //性別
-    final String[] ARRAY_GENDER = {"unknown","male","female"}
+	//ボールの種類(0:野生、4:敵)
+	final String[] ARRAY_BALL = {"Wild","Monster ball","Super ball","Master ball","Enemy"};
+	//ボールの画像(0:ボールなし（野生または敵）1:戦闘可能、2:瀕死状態）
+	final String[] ARRAY_IMG_BALL = {"","○","●"};
+	//性別
+	final String[] ARRAY_GENDER = {"Unknown","Male","Female"};
 	//技の効果
-    final double[] ARRAY_EFFECTIVE = {0.0, 0.5, 1.0, 2.0};
-    String name; //名前
+	final double[] ARRAY_EFFECTIVE = {0.0, 0.5, 1.0, 2.0};
+	String name; //名前
 	String nickname; //ニックネーム
-    String gender; //性別
+	String gender; //性別
 	String type; //タイプ
-    String ability; //特性
-    String ball; //ボール
-    String item; //もちもの
+	String ability; //特性
+	String ball; //ボール
+	String item; //もちもの
 	int dexNo; //ずかん番号
-    int level; //レベル
+	int level; //レベル
 	int hp; //体力
-    int hp_max; //最大HP
+	int hp_max; //最大HP
 	int exp; //経験値
-    double height; //高さ(m)
-    double weight; //重さ(kg)
-    Random rand; //乱数用
+	double height; //高さ(m)
+	double weight; //重さ(kg)
+	Random rand; //乱数用
 	
-	public Pokemon(String nickname) {
-        this.name = "Unknown";
+	Pokemon(String nickname) {
+		this.name = "Unknown";
 		this.nickname = nickname;
-        this.gender = ARRAY_GENDER[0];
-        this.type = ARRAY_TYPE[0];
-        this.ability = "Unknown";
-        this.ball = ARRAY_BALL[0];
-        this.item = "None";
-        this.id = 0;
+		this.gender = ARRAY_GENDER[0];
+		this.type = ARRAY_TYPE[0];
+		this.ability = "Unknown";
+		this.ball = ARRAY_IMG_BALL[0];
+		this.item = "None";
+		this.dexNo = 0;
 		this.level = 1;
-        this.hp_max = 1;
-        this.hp = this.hp_max;
+		this.hp_max = 1;
+		this.hp = this.hp_max;
 		this.exp = 0;
-        this.height = 0.1;
-        this.weight = 1.0;
-        this.rand = new Random();
+		this.height = 1.0;
+		this.weight = 1.0;
+		this.rand = new Random();
 	}
 
-    public String toString() {
-		String str = this.nickname + "(" + this.name + ") Lv." + this.level
-				+ "\ntype:" + this.type
-				+ "\nhp:" + this.hp + "/" + this.hp_max
-				+ "\nexp:" + this.exp;
+	public String toString() {
+		String str = this.ball + this.nickname + "(" + this.name + ") Lv." + this.level
+				+ "\nType: " + this.type
+				+ "\nHP: " + this.hp + "/" + this.hp_max
+				+ "\nExp.Points: " + this.exp;
 		return str;
 	}
 
-    //ステータスを見る
-	public void viewStatus() {
+	//ステータスを見る
+	void getStatus() {
 		System.out.println(this.toString());
 	}
 		
 	//逃げる
-	public void run() {
+	void run() {
 		System.out.println(this.nickname + " run away.");
 	}
 
-    //回復する
-	public void recover(){
+	//回復する
+	void recover(){
 		this.hp = this.hp_max;
+		System.out.println("Your " + this.nickname + " regained health!");
 	}
 
-    //抽象メゾット
+	//抽象メゾット
 	//戦う
 	abstract void attack(Pokemon p);
 	
