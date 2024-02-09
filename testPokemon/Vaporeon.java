8e8f987b5c5471ca8b1bc1bf7f69d45cfaf955ca
mow <154735003+J60116@users.noreply.github.com>
2024-02-09 13:16:09 +0900

    Update Vaporeon.java

--- testPokemon/Vaporeon.java
+++ testPokemon/Vaporeon.java
@@ -1,24 +1,25 @@
 package testPokemon;
 
-public class Vaporeon extends Eevee{ //シャワーズ
+public class Vaporeon extends Eevee{ 
+	//シャワーズ
+	static final String NAME = "Vaporeon";
 
-	static final String NAME = "Eevee";
+	public Vaporeon(){
+		this(NAME);
+	}
 
-    public Vaporeon(){
-        this(NAME);
-    }
-
-    public Vaporeon(String name){
-        super(name);
-        this.name = ARRAY_EVOLVED_NAME[0];
-        this.type = ARRAY_EVOLVED_TYPE[0];
-        this.ability = ARRAY_EVOLVED_ABILITY[0];
-        this.dexNo = ARRAY_EVOLVED_DEXNO[0];
-        this.hp_max = 130;
-        this.hp = this.hp.max
-        this.height = 1.0;
+	public Vaporeon(String name){
+		super(name);
+		this.name = super.ARRAY_EVOLVED_NAME[0];
+		this.type = super.ARRAY_EVOLVED_TYPE[0];
+		this.ability = ARRAY_EVOLVED_ABILITY[0];
+		this.ball = ARRAY_IMG_BALL[1];
+		this.dexNo = ARRAY_EVOLVED_DEXNO[0];
+		this.hp_max = 130;
+		this.hp = this.hp_max;
+		this.height = 1.0;
 		this.weight = 29.0;
-    }
+	}
 
 	@Override
 	void attack(Pokemon p) {
@@ -26,13 +27,13 @@ public class Vaporeon extends Eevee{ //シャワーズ
 		p.hp -= 10;
 	}
 
-    void HydroPump(Pokemon p){
-        System.out.println(this.name + " used Hydro Pump!");
+	void HydroPump(Pokemon p){
+		System.out.println(this.name + " used Hydro Pump!");
 		p.hp -= 20;
-    }
+	}
 
 	@Override
-	void viewStatus() {
+	void getStatus() {
 		System.out.println(this.toString());
 	}
 }
