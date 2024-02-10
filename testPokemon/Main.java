fae025e8f16974da50b17e7deb4cc118ae4db6fc
mow <j16.gleen8@gmail.com>
2024-02-10 23:07:53 +0900

    Pokemonコンストラクタの引数をボールに変更、getPokemon()の引数にボールを追加、giveNickname()の修正、setPocket()の作成

--- testPokemon/Main.java
+++ testPokemon/Main.java
@@ -5,31 +5,31 @@ public class Main {
 	public static void main(String[] args) {
 		//ポケモントレーナーの生成
 		User user = new User();
-
+	
 		//いま所持しているポケモン
-		user.pocket[0] = new Eevee();
-		user.pocket[1] = new Eevee("Brown");
-		user.pocket[2] = new Vaporeon("Blue");
+		user.setPocket(0,new Eevee("MonsterBall"));
+		user.setPocket(1,new Eevee("MonsterBall"));
+		user.setPocket(2,new Vaporeon("MonsterBall"));
 		
-		//ポケモンが現れた
+		//イーブイが現れた
 		Pokemon eevee = new Eevee();
-		
-		//ポケモンを捕まえた
-		user.getPokemon(eevee);
+
+		//ポケモンをスーパーボールで捕まえる
+		user.getPokemon(eevee, "SuperBall");
 
 		//所持しているポケモンのステータスを見る
 		user.viewPartyStatus();
 
 		//ポケモンセンターに行く
 		user.visitPokemonCenter();
-		
+
 		//2番目と3番目のポケモンに「かみなりのいし」を渡す
-		user.givePokemonItem(user.pocket[1], "ThunderStone");
-		user.givePokemonItem(user.pocket[2], "ThunderStone");
-		
+		user.giveItem(user.getPocket()[1], "ThunderStone");
+		user.giveItem(user.getPocket()[2], "ThunderStone");
+
 		//所持しているポケモンのステータスを見る
 		user.viewPartyStatus();
-		
+
 	}
 
 }
--- testPokemon/Vaporeon.java
+++ testPokemon/Vaporeon.java
@@ -5,11 +5,11 @@ final class Vaporeon extends Eevee{
 	static final String NAME = "Showers";
 
 	public Vaporeon(){
-		this(NAME);
+		this(ARRAY_IMG_BALL[0]);
 	}
 
-	public Vaporeon(String nickname){
-		super(nickname);
+	public Vaporeon(String ball){
+		super(ball);
 //		this.dexNo = super.ARRAY_EVOLVUTION[0][0];
 //		this.name = super.ARRAY_EVOLVUTION[0][1];
 //		this.type = super.ARRAY_EVOLVUTION[0][2];
@@ -19,10 +19,10 @@ final class Vaporeon extends Eevee{
 //		this.weight = super.ARRAY_EVOLVUTION[0][6];
 		this.dexNo = ARRAY_EVOLVED_DEXNO[0];
 		this.name = ARRAY_EVOLVED_NAME[0];
+		this.nickname = this.name;
 		super.setGender();
 		this.type = ARRAY_EVOLVED_TYPE[0];
 		this.ability = ARRAY_EVOLVED_ABILITY[0];
-		this.ball = ARRAY_IMG_BALL[1];
 		this.height = ARRAY_EVOLVED_HW[0][0];
 		this.weight = ARRAY_EVOLVED_HW[0][1];
 		this.hp_max = ARRAY_EVOLVED_MAXHP[0];
